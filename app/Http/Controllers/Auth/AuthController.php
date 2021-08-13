dafdf5c460c7d40bc53805bf472a0616331528c6
onoy001 <onoy001@yahoo.co.jp>
2021-08-13 10:10:29 +0900

    最終変更

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,6 +10,10 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
     /**
      * @return View
      */
@@ -26,28 +30,23 @@ class AuthController extends Controller
     {
         $credentials = $request -> only('email', 'password');
     
-        $user = User::where('email', '=', $credentials['email'])->first();
+        $user = $this->user->getUserByEmail($credentials['email']);
 
         if(!is_null($user)){
-            if($user->locked_flg === 1){
+            if($this->user->isAccountLocked($user)){
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされています。',
                 ]);
             }
             if(Auth::attempt($credentials)){
                 $request->session()->regenerate();
-                if($user->error_count > 0){
-                    $user->error_count = 0;
-                    $user->save();
-                }
+                $this->user->resetErrorCount($user);
                 
                 return redirect()->route('home')->with('success','ログイン成功しました！');
             }
 
-            $user->error_count = $user->error_count + 1;
-            if($user->error_count > 5){
-                $user->locked_flg = 1;
-                $user->save();
+            $user->error_count = $this->user->addErrorCount($user->error_count);
+            if($this->user->lockAccount($user)){
                 return back()->withErrors([
                     'danger' => 'アカウントロックされました。解除したい場合は運営者に連絡ください。',
                 ]);
